fd362a72296ebfe7d0c31eabff5d06f8daeb9f5d
Sindy Soria Francis <ing.ssoria@gmail.com>
2023-11-06 23:33:54 -0300

    Init GCP terraform

---
+++ TP_8/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.80.0"
+    }
+  }
+}
+
+provider "google" {
+
+  project = var.PROJECT_ID
+  region  = var.REGION
+}
